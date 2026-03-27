3535d012ac8edbfb06bdb7163ed946445ff731aa
mdetoeuf <morgane.detoeuf@gmail.com>
2026-03-27 14:41:41 +0100

    create a template for new functions

---
+++ functions/0_template_function.R
@@ -0,0 +1,18 @@
+#** TITLE *
+
+#> Author      : Morgane de Toeuf
+#> Version     : 2026-03-27
+#> Purpose     : 
+#> Returns     : 
+#>               
+#** To Do: specify particularities *
+#*
+#** -  *
+#** -  *
+#*
+
+#** Example of parameters  *
+
+#> 
+#> 
+#> 
